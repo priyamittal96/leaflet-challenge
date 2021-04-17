b7426ffc057b332b05fabd9f7c5b01d14ecb59f0
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-04-17 10:03:44 -0700

    Background layer

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoic2hhbnRpcHJpeWEzMCIsImEiOiJja242dGRrYXUwaGR1Mm9zMWd5aXNyeDYxIn0.pMHgT13s9qDCY0Bns8oMqQ";
---
+++ static/js/logic.js
@@ -0,0 +1,19 @@
+console.log("working");
+
+
+// title layer as the background of the map
+let streets = L.tileLayer('https"//api.mapbox.com/styles/v1/mapbox/streets-v11/{z}/{x}/{y}?access_token={accessToken}',{
+    attribution: 'Map data',
+    maxZoom: 18,
+    accessToken: API_KEY
+})
+
+let map = L.map('mapid',{
+    center: [40.7,-94.5],
+    zoom: 3,
+    layers:[streets]
+})
+
+let baseMap = {
+    
+}
