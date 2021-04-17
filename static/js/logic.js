7660bccb9a78dea5cf4c81c621a303aab2aae803
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-04-17 11:07:54 -0700

    basemap and backgrounds

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,6 +15,13 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     accessToken: API_KEY
 })
 
+// second tile layer for background
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+    attribution: 'Map data',
+    maxZoom: 18,
+    accessToken: API_KEY
+})
+
 let map = L.map('mapid',{
     center: [40.7,-94.5],
     zoom: 3,
@@ -23,6 +30,8 @@ let map = L.map('mapid',{
 
 let baseMap = {
     "Streets": streets,
+    "Satellite": satelliteStreets,
+    "Light": light
 }
 
 // L.control.layers(baseMap, overlays)
